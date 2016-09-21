b9df83a0dc5d03fea511020d64a24262cf2331fb
crisenchou <269298762@qq.com>
2016-09-21 12:28:00 +0800

    rsa test

--- dailyPratice/php/RSA.php
+++ dailyPratice/php/RSA.php
@@ -9,14 +9,24 @@ class RSA{
     private $__P;
     private $__Q;
     
-    public function __construct($e)
+    public function __construct()
     {
         // $this->__P = $this->getPrime();
         // $this->__Q = $this->getPrime();
-        // $this->__N = gmp_mul($this->__P,$this->__Q);
-        // $this->__E = $e;
-        // $THIS->__d = $this->getD();
         
+        // $this->__P = 3;
+        // $this->__Q = 5;
+        
+        // $this->__N = $this->__P*$this->__Q;
+        // $this->__E = 3;
+        // $d = $this->getD();
+        // $this->__D = $d;
+        
+        $this->__P = 7;
+        $this->__Q = 5;
+        $this->__N = 143;
+        $this->__E = 7;
+        $this->__D = 103;
     }
     
     //chr() ASCII->char   ord()   char->ASCII
@@ -35,7 +45,7 @@ class RSA{
     
     
     //在区域内获得一个随机素数
-    public function getPrime($from=111111, $to=999999)
+    public function getPrime($from=10, $to=50)
     {
         $randNum = mt_rand($from, $to);
         
@@ -58,88 +68,151 @@ class RSA{
         return true;
     }
 
-    //判断两个数属否互质
-    private function isRelativePrime($min, $max)
+    //gmp_init
+    //求n的欧拉数
+    public function eular($n)
     {
-
-        if(gmp_mod($max,$min) ==0){
+        $ret=1;
+        for($i=2;$i*$i<=$n;$i++)
+        {
+            if($n%$i==0)
+            {
+                $n/=$i;
+                $ret*=$i-1;
+                while($n%$i==0)
+                {   $n/=$i;
+                    $ret*=$i;
+                }
+            }
+        }
+        if($n>1) $ret*=$n-1;
+        return $ret;
+    }
+    
+    
+    public function getD()
+    {
+        
+        $k = ($this->__P-1)*($this->__Q-1);
+        
+        
+        $this->__E = $this->getE($k);
+        
+        // echo $this->__E;exit;
+        
+        //$this->__E = $k-1;
+        
+        //$eulerNum = $this->eular($this->__N);
+        
+        for($d=1;$d<=$k;$d++){
+            $pow =  $this->__E * $d;
+            if($pow % $k == 1){
+                echo $d;
+                break;
+            }
+        }
+        return $d;
+    }
+    
+    
+    public function isRelativePrime($min,$max){
+        if($max % $min ==0){
             return false;
         }
         
-        for($i=2;$i<=$min;$i++){
-            if( gmp_mod($max,$i) == 0 && gmp_mod($min,$i) == 0){
+        for($i=2;$i<$min;$i++){
+            if($max%$i==0 && $min%$i==0){
                 return false;
+                break;
             }
         }
+        
         return true;
     }
     
-    //求n的欧拉数
-    public function getEulerNum($n){
-        $sum=1;
-        for($i=1;$i<$n;$i++){
-            if($this->isRelativePrime($i,$n) === true){
-                $sum += 1;;
-            }
-        }
-        return $sum;
-    }
-    
-    public function getD(){
-        $flag = true;
-        
-        $num = gmp_mul($this->__P-1,$this->__Q-1);
-        
-        
-        $eulerNum = $this->getEulerNum($num);
-        
-        var_dump($eulerNum);exit;
-        
-        while(true){
-            $d = 100;
-            
-            if( gmp_mod(gmp_pow($this->__E,$d) % $eulerNum) ==1 ){
+    public function getE($n){
+        for($i=2;$i<$n;$i++){
+            if($this->isRelativePrime($i,$n)){
+                return $i;
                 break;
             }
-            $d++;
         }
-        return $D;
+        return false;
+    }
+    
+    public function check(){
+        for($i=1;$i<$this->__N;$i++){
+            $encrypt = $this->encrypt($i);
+            $message = $this->decrypt($encrypt);
+            echo "the encrypt is ".$encrypt;
+            echo PHP_EOL;
+            echo "the message is ".$message;
+            echo PHP_EOL;
+        }
+    }
+    
+    //chr() ASCII->char   ord()   char->ASCII
+    public function enctyptMessage(){
+        $message = "abc";
+        $encrypt = [];
+        for($i=0;$i<strlen($message);$i++){
+            $num = ord($message{$i});
+            //$ord = (int)$this->encrypt($num);
+            $encrypt[$i] = $this->encrypt($num);
+        }
         
+        $str = "";
+        foreach($encrypt as $val){
+            $ord = (int)$this->decrypt($val);
+            $str .= chr($ord);
+        }
+        
+        echo $str;exit;
+        var_dump($encrypt);exit;
+        
+        $this->checkstring($encrypt);
+        
+        var_dump($encrypt);
+        exit;
+        return $encrypt;
+    }
+    
+    
+    public function checkstring($str){
+        for($i=0;$i<strlen($str);$i++){
+           echo ord($str{$i});
+        }
+    }
+    
+    public function test(){
+        $this->__P = $this->getPrime();
+        $this->__Q = $this->getPrime();
+        $this->__P = 11;
+        $this->__Q = 13;
+        $this->__N = ($this->__P)*($this->__Q);
+        //$this->__E = 3;
+        $d = $this->getD();
+        $this->__D = $d;
     }
     
 }
 
 
-function isPrime($n)
-{
-    for($i=2;$i*$i<=$n;$i++){
-        if($n%$i==0){
-            printf("%u\n", $i);
-            return false;
-            break;
-        }
-    }
-    return true;
-}
+$rsa = new RSA();
 
-//echo ~18446744072499298262;
-//echo PHP_EOL;
+$en = $rsa->enctyptMessage();
+echo $en;exit;
+$rsa->check();
 
-//var_dump(isPrime(1625256759));
-
+// $rsa->test();var_dump($rsa);
 
 
 exit;
 
-// $rsa = new RSA(3);
-
-// var_dump($rsa->isPrime(1210253353));exit;
 
 
-// var_dump($rsa);exit;//加密数据不能大于N的值
-// $message = 14;
-// $encrypt = $rsa->encrypt($message);
-// $message = $rsa->decrypt($encrypt);
-// echo "the encrypt is ".$encrypt;
-// echo PHP_EOL;
-// echo "the message is ".$message;
+
+
+
+
+

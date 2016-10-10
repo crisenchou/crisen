8f06d3eee46a941b1b144f77640099407ebf8659
crisenchou <269298762@qq.com>
2016-10-10 18:04:34 +0800

    c practice

--- dailyPratice/C/practice/args.c
+++ dailyPratice/C/practice/args.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc,char* argv[])//整数类型主函数(整数类型统计参数个数,字符类型*数组指针至字符[])
+int main(int argc,char *argv[])//整数类型主函数(整数类型统计参数个数,字符类型*数组指针至字符[])
 {
-    printf("%d\n",argc);//格式化输出
-    while(argc)//当(统计参数个数)
-    printf("%s\n",argv[--argc]);//格式化输出
+
+    int c;
+    /* process arguments */
+    while (-1 != (c = getopt(argc, argv,
+          "a"  /* access mask for unix socket */
+          "b"  /* enable admin shutdown commannd */
+          "c:"  /* TCP port number to listen on */
+        ))) {
+            switch(c){
+                case 'a' :
+                    printf("the param is a\n");
+                    break;
+                case 'b' :
+                    printf("the param is b\n");
+                    break;
+                case 'c' :
+                    //printf("option=c, optopt=%c, optarg=%s\n", optopt, optarg);
+                    printf("the param c is %s\n", optarg);
+                    break;
+                case ':' :
+                    printf("missing value");
+                    break;
+                case '?' :
+                    printf("illegal params");
+                    break;
+                default : 
+                    printf("illegal params");
+                    return 1;
+            }
+        }
     return 0;//返回0;正常退出
 }

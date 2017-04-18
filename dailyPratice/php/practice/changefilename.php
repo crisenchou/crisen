0346ce0285e2bdb1c31ec30cf29257762beebcb1
crisenchou <269298762@qq.com>
2017-04-18 12:18:38 +0800

    change mynote

--- dailyPratice/php/practice/changefilename.php
+++ dailyPratice/php/practice/changefilename.php
@@ -13,27 +13,23 @@ function changeSuffix($dir = '.', $find = 'html', $replace = 'blade.php'){
             while (($file = readdir($dh)) !== false){
                 if($file != '.' && $file != '..'){
                     if(is_dir($file)){
-                        changeSuffix($file);
+                        changeSuffix($file, $find, $replace);
                     }else{
                         $file = $dir.'/'.$file;
-                        $filename = str_replace($find,$replace,$file);
+						if(!$find){
+							$filename =  $file.$replace;
+						}else{
+							$filename = str_replace($find,$replace,$file);
+						}
                         rename($file,$filename);
                     }
                 }
-                // if(is_dir($file)){
-                    // return changeSuffix($file)
-                // }else{
-                    // $filename = str_replace('html','blade.php',$file);
-                // rename($file,$filename);
-                // }
             }
         closedir($dh);
         }
     }
 }
 
-changeSuffix();
+changeSuffix('.','txt','md');
 
 echo 'ok';
-
-

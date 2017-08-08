b9c5bba37e56ac97d0dc7f1d3be065d39258e224
crisenchou <269298762@qq.com>
2017-08-08 16:33:13 +0800

    remove elements

--- dailyPratice/C/algorithm/removeElement.c
+++ dailyPratice/C/algorithm/removeElement.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void removeElement(int *arr, int* length, int val);
-int findElement(int *arr, int* length, int val);
+
+int removeElement(int *arr, int length, int val);
 
 
 void printArr(int *arr, int size){
@@ -13,32 +13,23 @@ void printArr(int *arr, int size){
 }
 
 int main(){
-    int length=6;
-    int arr[6] = {1,2,3,4,5,3};
-    int l = findElement(arr,&length,3);
-    printf("%d\n",l);
-    printf("the length is %d\n",length);
-    printArr(arr,length);
+    int length=11;
+    int arr[11] = {2,1,3,0,1,1,0,3,3,1,3};
+    int len = removeElement(arr,length,3);
+    printf("%d\n",len);
+    printArr(arr,len);
     getche();
 }
 
-int findElement(int *arr, int* length, int val){
-    int i,len;
-    for(i=0;i<*length;i++){
-        if(arr[i] == val){
-            removeElement(arr, length, i);
+int removeElement(int* nums, int numsSize, int val) {
+    int i,j,len;
+    for(i=numsSize-1;i>=0;i--){
+        if(nums[i] == val){
+            numsSize--;
+            for (j = i; j < numsSize; j++){
+                nums[j] = nums[j+1];
+            }
         }
     }
-    len= *length;
-    return len;
-}
-
-void removeElement(int *dat, int *len, int pos){
-    int i;
-    if (pos < 0 || pos >= *len)
-        return;
-    
-    (*len)--;
-    for (i = pos; i < *len; i++)
-        dat[i] = dat[i+1];
+    return numsSize;
 }

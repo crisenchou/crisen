70576cc8b2106fe66c989b88baf0b223018763de
crisenchou <269298762@qq.com>
2017-12-06 10:59:00 +0800

    boot

--- dailyPratice/assembly/bootloader.asm
+++ dailyPratice/assembly/bootloader.asm
@@ -1,4 +1,9 @@
+;%define _BOOT_DEBUG_
+%ifdef _BOOT_DEBUG_
+    org 0100h
+%else
     org 07c00h
+%endif
     mov ax, cs
     mov ds, ax
     mov es, ax
@@ -7,7 +12,7 @@
 DispStr:
     mov ax, BootMessage
     mov bp, ax
-    mov cx, 16
+    mov cx, 12
     mov ax, 01301h
     mov bx, 000ch
     mov dl, 0
